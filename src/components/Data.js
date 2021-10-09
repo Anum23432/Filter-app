d6ca260755ad48b6860890191a90af90fd764058
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-10-09 20:58:42 +0500

    filter app

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,3 +1,4 @@
+import img1 from './images/sam2.jpg';
 const data = 
     
     [
@@ -5,7 +6,7 @@ const data =
           "category": "Samsung",
           "id": 1,
           "model": "Galaxy Z flip3",
-          "picture": '../images/sam2.jpg',
+          "picture": img1,
           "body": "Plastic front, glass back (Gorilla Glass Victus), aluminum frame",
           "display": "1080 x 2640 pixels (~426 ppi density)",
           "memory": "128GB 8GB RAM, 256GB 8GB RAM",

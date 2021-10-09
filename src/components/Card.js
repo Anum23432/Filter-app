8cb8062e10c19479b9307ca7fe21be1a2dbc8a9b
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-10-09 13:10:08 +0500

    filter app

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import StarRating from "./StarRating";
 
 
 
@@ -11,14 +12,15 @@ function Card(props) {
     return (
         <>
             <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
-                <div class="card p-0 overflow-hidden h-100 shadow" >
-                    <img src={props.image} className="card-img-top" alt="..." />
+                <div class="card p-0 overflow-hidden h-100 " >
+                    <img src={props.picture} className="card-img-top" alt="mobile" /><hr/>
                     <div className="card-body">
-                        <h5 class="card-title">{props.category}</h5>
-                        {/* <p className="card-text">{props.description}.</p> */}
+                        <div className="abc"><StarRating/></div> <hr/>                      
+                         <h5 class="card-title">{props.category}</h5><hr/>
+                        <h5 class="card-title">{props.model}</h5>
                         <Route>
 
-                            <Link to={props.link} class="btn btn-primary">Go somewhere</Link>
+                            <Link to={props.link} class="btn btn-primary">Show More!</Link>
                         </Route>
                     </div>
                 </div>

71ba81f1678cd6c77469cc81564a6a012984b0af
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-10-09 08:57:41 +0500

    filter app

---
+++ src/components/Animation.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Animation() {
+    return(
+        <>
+    
+	<div class="overlay"></div>
+
+<div class="text">
+	<div class="wrapper">
+		<div id="L" class="letter">R</div>
+		<div class="hadow">R</div>
+	</div>
+	<div class="wrapper">
+		<div id="I" class="letter">E</div>
+		<div class="hadow">E</div>
+	</div>
+	<div class="wrapper">
+		<div id="G" class="letter">V</div>
+		<div class="hadow">V</div>
+	</div>
+	<div class="wrapper">
+		<div id="H" class="letter">I</div>
+		<div class="hadow">I</div>
+	</div>
+	<div class="wrapper">
+		<div id="T" class="letter">E</div>
+		<div class="hadow">E</div>
+	</div>
+	<div class="wrapper">
+		<div id="N" class="letter">W</div>
+		<div class="hadow">W</div>
+	</div>
+	<div class="wrapper">
+		<div id="E" class="letter">S</div>
+		<div class="hadow">S</div>
+	</div>
+	<div class="wrapper">
+		<div id="S" class="letter">!</div>
+		<div class="hadow">!</div>
+	</div>
+</div>
+        </>
+    )
+}
+export default Animation;
